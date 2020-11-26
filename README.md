05989e84916d296c1112a15c8ffefaa606d31da2
bryan <bryanibit@gmail.com>
2020-11-26 15:04:24 +0800

    reconstruct

--- README.md
+++ README.md
@@ -3,8 +3,9 @@
 Input a serial of GPS points and choose some points from them and save them.
 Shortcut keyword:
 1. Esc: quit
-2. w: save chose points
+2. w: save chosen points
 3. r: remove the last points
+4. m: change mode between *free* and *constraint*
 
 # Note
 
--- main.cc
+++ main.cc
@@ -33,18 +33,89 @@ public:
             x(x__), y(y__), meterx(meterx__), metery(metery__), offsetx(offsetx__), offsety(offsety__){}
 };
 
-std::vector<int> split(string &s, const string &delimiter){
-    int pos = s.find_first_not_of(delimiter, 0);
-    int after = s.find_first_of(delimiter, pos);
-    vector<int> res;
-    int temp;
-    while(pos != string::npos || after != string::npos){
-        temp = stoi(s.substr(pos, after - pos));
-        res.push_back(temp);
-        pos = s.find_first_not_of(delimiter, after);
-        after = s.find_first_of(delimiter, pos);
+namespace process {
+    std::vector<int> split(string &s, const string &delimiter) {
+        int pos = s.find_first_not_of(delimiter, 0);
+        int after = s.find_first_of(delimiter, pos);
+        vector<int> res;
+        int temp;
+        while (pos != string::npos || after != string::npos) {
+            temp = stoi(s.substr(pos, after - pos));
+            res.push_back(temp);
+            pos = s.find_first_not_of(delimiter, after);
+            after = s.find_first_of(delimiter, pos);
+        }
+        return res;
     }
-	return res;
+
+
+    void LonLat2Offset(vector<PosXY> &offset, const PosXY &origin, const vector<vector<int> > &lines) {
+        if (lines.empty())
+            return;
+        auto pre_line = lines[0];
+        int pre_line_x;
+        int pre_line_y;
+        Mercator::LB2XY(lines[0][0] * 0.000001 * M_PI / 180.0,
+                        lines[0][1] * 0.000001 * M_PI / 180.0, pre_line_x, pre_line_y);
+        double pre_x = pre_line_x * 0.1 - origin.meterx;
+        double pre_y = pre_line_y * 0.1 - origin.metery;
+        offset.emplace_back(lines[0][0], lines[0][1], pre_line_x * 0.1, pre_line_y * 0.1, pre_x, pre_y);
+
+        for (int i = 0; i < lines.size(); ++i) {
+            int x;
+            int y;
+            if (lines[i].size() == 4) {
+                Mercator::LB2XY(lines[i][0] * 0.000001 * M_PI / 180.0,
+                                lines[i][1] * 0.000001 * M_PI / 180.0, x, y);
+            }
+            double offset_x = x * 0.1 - origin.meterx;
+            double offset_y = y * 0.1 - origin.metery;
+            // filter some points
+            if (sqrt(pow(offset_x - pre_x, 2) + pow(offset_y - pre_y, 2)) >= kDisPoints) {
+                offset.emplace_back(lines[i][0], lines[i][1], x * 0.1, y * 0.1, offset_x, offset_y);
+                pre_x = offset_x;
+                pre_y = offset_y;
+            }
+        }
+    }
+
+    void ShowOffsetDebug(const vector<PosXY> &offset) {
+        for (int i = 0; i < offset.size(); ++i) {
+            std::cout << offset.at(i).offsetx << ", " << offset.at(i).offsety << std::endl;
+        }
+    }
+
+    int ExtractFullRoad(const string &file_path, PosXY &origin, vector<PosXY> &offset) {
+        std::vector<std::vector<int> > lines;
+        std::ifstream infile;
+        infile.open(file_path);
+        string s;
+        while (infile >> s)
+            lines.push_back(process::split(s, ","));
+        infile.close();
+        // lines: 10234566 3994534 0 2
+        if (lines.size() < 10)
+            return -1;
+
+        origin.x = lines[0].at(0);
+        origin.y = lines[0].at(1);
+        for (int i = 1; i < 10; ++i) {
+            origin.x = (lines[i][0] + origin.x) / 2.0;
+            origin.y = (lines[i][1] + origin.y) / 2.0;
+        }
+        // origin meterx and y
+        int meterx;
+        int metery;
+        Mercator::LB2XY(origin.x * 0.000001 * M_PI / 180.0,
+                        origin.y * 0.000001 * M_PI / 180.0, meterx, metery);
+        origin.meterx = meterx * 0.1;
+        origin.metery = metery * 0.1;
+        origin.offsetx = 0;
+        origin.offsety = 0;
+        process::LonLat2Offset(offset, origin, lines);
+        std::cout << offset.size() << std::endl;
+        return 0;
+    }
 }
 
 class ShowMap{
@@ -181,81 +252,10 @@ public:
     }
 };
 
-void LonLat2Offset(vector<PosXY> &offset, const PosXY &origin, const vector<vector<int> > &lines){
-    if(lines.empty())
-        return;
-    auto pre_line = lines[0];
-    int pre_line_x;
-    int pre_line_y;
-    Mercator::LB2XY(lines[0][0] * 0.000001 * M_PI / 180.0,
-                    lines[0][1] * 0.000001 * M_PI / 180.0, pre_line_x, pre_line_y);
-    double pre_x = pre_line_x * 0.1 - origin.meterx;
-    double pre_y = pre_line_y * 0.1 - origin.metery;
-    offset.emplace_back(lines[0][0], lines[0][1], pre_line_x * 0.1, pre_line_y * 0.1, pre_x, pre_y);
-
-    for(int i = 0; i < lines.size(); ++i){
-        int x; int y;
-        if(lines[i].size() == 4){
-            Mercator::LB2XY(lines[i][0] * 0.000001 * M_PI / 180.0,
-                            lines[i][1] * 0.000001 * M_PI / 180.0, x, y);
-        }
-        double offset_x = x * 0.1 - origin.meterx;
-        double offset_y = y * 0.1 - origin.metery;
-        // filter some points
-        if(sqrt(pow(offset_x - pre_x, 2) + pow(offset_y - pre_y, 2)) >= kDisPoints) {
-            offset.emplace_back(lines[i][0], lines[i][1], x * 0.1, y * 0.1, offset_x, offset_y);
-            pre_x = offset_x;
-            pre_y = offset_y;
-        }
-    }
-}
-
-void ShowOffsetDebug(const vector<PosXY> &offset){
-    for(int i = 0; i < offset.size(); ++i){
-        std::cout << offset.at(i).offsetx << ", " << offset.at(i).offsety << std::endl;
-    }
-}
-
 int main(){
-    std::vector<std::vector<int> > lines;
-    std::ifstream infile;
-    infile.open("../res.txt");
-    string s;
-    while(infile >> s)
-        lines.push_back(split(s, ","));
-    infile.close();
-    // lines: 10234566 3994534 0 2
-    if(lines.size() < 10)
-        return -1;
     PosXY origin;
-    origin.x = lines[0].at(0);
-    origin.y = lines[0].at(1);
-    for(int i = 1; i < 10; ++i){
-        origin.x = (lines[i][0] + origin.x) / 2.0;
-        origin.y = (lines[i][1] + origin.y) / 2.0;
-    }
-    // origin meterx and y
-    int meterx;
-    int metery;
-    Mercator::LB2XY(origin.x * 0.000001 * M_PI / 180.0, 
-            origin.y * 0.000001 * M_PI / 180.0, meterx, metery);
-    origin.meterx = meterx * 0.1;
-    origin.metery = metery * 0.1;
-    origin.offsetx = 0;
-    origin.offsety = 0;
     vector<PosXY> offset;
-    LonLat2Offset(offset, origin, lines);
-    std::cout << offset.size() << std::endl;
-
-//    std::ofstream onfile;
-//    onfile.open("RNDFFull.txt", std::ios::out);
-//    if(onfile.is_open()){
-//        for(int i = 0; i < offset.size(); ++i)
-//            onfile<<std::fixed<<std::setprecision(6)<< offset[i].x * 0.000001 << " "
-//                  << std::fixed<<std::setprecision(6)<< offset[i].y * 0.000001<< " 0 2" <<endl;
-//    }
-//    onfile.close();
-
+    process::ExtractFullRoad("../res.txt", origin, offset);
 //    ShowOffsetDebug(offset);
     ShowMap sm(kWIDTH, kHEIGHT, kRES, origin);
     sm.DrawMap(offset);
